0d75ac490aa4fbb92648dac28a9695be7fb3aa9c
DalenMax <mxt.tung@gmail.com>
2026-05-02 13:29:28 +0700

    chore(tracing): remove legacy tracing folder and stylesheet

--- desktop-ui/src/features/plugins/coding-memory/tracing/event-cards/StepInterruptedCard.tsx
+++
@@ -1,8 +0,0 @@
-import type { TraceEvent } from "../types";
-
-interface Props { event: TraceEvent; }
-
-export function StepInterruptedCard({ event }: Props) {
-  const reason = (event.payload as any)?.reason ?? "interrupted";
-  return <div className="tracing-evcard tracing-evcard--interrupted">{reason}</div>;
-}
